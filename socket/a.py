2510d02789856b77ed8dc0c9082d6ad037c13301
hinatiki-main <hinata-daichi@outlook.jp>
2024-10-18 22:19:30 +0900

    修正

--- socket/a.py
+++ socket/a.py
@@ -6,8 +6,9 @@ async def echo(websocket, path):
         print(f"受け取ったメッセージ: {message}")
 
 async def main():
-    async with websockets.serve(echo, "localhost", 5011):
-        print("サーバーが起動したで、ポート5011で待っとるで！")
+    port = 5011
+    async with websockets.serve(echo, "localhost", port):
+        print(f"ポート{port}で待機")
         await asyncio.Future()  # 無限に待つ
 
 if __name__ == "__main__":
